c5e0fe664ac05d099d1b76983b44943add279f90
Jefferson Setiawan <jefferson.setiawan@tokopedia.com>
2023-11-23 10:19:14 +0700

    Initial Commit

---
+++ TCADevcamp/TCADevcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCADevcampApp.swift
+//  TCADevcamp
+//
+//  Created by jefferson.setiawan on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TCADevcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
