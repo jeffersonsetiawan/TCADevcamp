ddeddd6fb40fbc43692c0b102cf94d1ae158308d
Jefferson Setiawan <jefferson.setiawan@tokopedia.com>
2023-12-03 09:51:27 +0700

    init

--- TCADevcamp/TCADevcampApp.swift
+++ TCADevcamp/TCADevcampApp.swift
@@ -5,7 +5,26 @@
 //  Created by jefferson.setiawan on 23/11/23.
 //
 
+import ComposableArchitecture
 import SwiftUI
+
+struct DevcampApp: Reducer {
+    struct State: Equatable {
+        var a = 0
+    }
+    
+    enum Action {
+        case didAppear
+    }
+    
+    func reduce(into state: inout State, action: Action) -> Effect<Action> {
+        switch action {
+        case .didAppear:
+            state.a += 1
+            return .none
+        }
+    }
+}
 
 @main
 struct TCADevcampApp: App {
